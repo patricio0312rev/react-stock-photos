3387ddf07d178eb9c6794185ea0050d5f9e00b59
Juan Patricio Marroquin <review0312oficial@gmail.com>
2022-01-18 16:55:45 -0500

    Configurando hooks y conectando a la API de Unsplash

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const clientId = `?client_id=${process.env.REACT_APP_UNSPLASH_KEY}`;
 const mainUrl = `${process.env.REACT_APP_UNSPLASH_URI}/photos`;
 const searchUrl = `${process.env.REACT_APP_UNSPLASH_URI}/search/photos`;
 
 function App() {
+  const [loading, setLoading] = useState(false);
+  const [photos, setPhotos] = useState([]);
+
+  const fetchImages = async () => {
+    let url;
+
+    url = `${mainUrl + clientId}`
+
+    try {
+      let response = await fetch(url);
+      let data = await response.json();
+      
+      setPhotos(data);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    fetchImages()
+  }, []);
   return (
     <div className="App">
       <div>Hello world</div>
