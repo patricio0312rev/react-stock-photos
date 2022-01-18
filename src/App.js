d31ed3d50674be5af24a6433e6a947acf1ba6313
Juan Patricio Marroquin <review0312oficial@gmail.com>
2022-01-18 16:48:39 -0500

    Agregando const de clientId

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 
+const clientId = `?client_id=${process.env.REACT_APP_UNSPLASH_KEY}`;
 const mainUrl = `${process.env.REACT_APP_UNSPLASH_URI}/photos`;
 const searchUrl = `${process.env.REACT_APP_UNSPLASH_URI}/search/photos`;
 
